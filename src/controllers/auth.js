7971625e7ca99009528bf1d086056760d3b7bd62
Singh1289 <singh.1289abhi@gmail.com>
2024-08-06 11:28:21 +0530

    regex change

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,7 +8,9 @@ import { send_email, send_message_to_phone } from "../utils/send_messages.js";
 import { combineTableNames } from "sequelize/lib/utils";
 
 const email_regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
-const phone_number_regex = /^(\+?\d{1,2}\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$/;
+//   /^(\+?\d{1,2}\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$/ 
+//   /^(\+91|\+91\-|0)?[789]\d{9}$/
+const phone_number_regex = /^[0]?[789]\d{9}$/;
 
 export const generate_otp_register = async (data, cb) => {
   try {
